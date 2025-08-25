25847702a6cdda76410d5e768410b5faefd21fa6
Enzo R. Gross Bangel <enzo.bangel@duque.g12.br>
2025-08-25 19:45:53 -0300

    primeiro commit da aplicacao

---
+++ aula04/src/main/java/com/enzo/aula04/Aula04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.enzo.aula04;
+
+/**
+ *
+ * @author 05675
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
